0046b89b04a5524a1cd288498cf9c8ee3e33b407
Silent Dev <rez_a@outlook.com>
2019-05-06 15:16:19 -0400

    Now able to push the data onto the api route '/api/friends'

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,13 @@ var friends = require('../data/friends.js');
 // The app here is the app we made in the 
 // server.js file. (var app = express();)
 module.exports = function (app) {
+
     app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
 
     app.post('/api/friends', function (req, res) {
         console.log(req.body);
+        friends.push(req.body);
     })
 }
