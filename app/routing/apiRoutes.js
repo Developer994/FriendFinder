e76967d977cbd366f7820e1fc29c44f40b92603b
Silent Dev <rez_a@outlook.com>
2019-05-06 13:27:04 -0400

    Able to console.log from the client side

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,14 @@
 var friends = require('../data/friends.js');
-
+// var bodyParser = require('body-parser')
 
 // The app here is the app we made in the 
 // server.js file. (var app = express();)
 module.exports = function (app) {
-    app.get('/api/survey', function (req, res) {
+    app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
+
+    app.post('/api/friends', function (req, res) {
+        console.log(req.body);
+    })
 }
