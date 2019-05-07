0489e3395bed163baaac417a12681ae944965bd4
Silent Dev <rez_a@outlook.com>
2019-05-07 15:56:40 -0400

    Changed images dir.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "friend1",
-        Link: "https://www.google.ca/search?hl=en&tbm=isch&source=hp&biw=1517&bih=741&ei=-MjQXNanCIuytQWFlKuoAQ&q=cute+turtle&oq=cute+turtle&gs_l=img.3..0l10.874.2880..3028...0.0..0.74.707.11......0....1..gws-wiz-img.....0.3FLv83WeWUA#imgrc=JdJrVgbwC49C-M:",
+        Link: "/public/Hams.jpg",
         score: [
             "5",
             "2",
@@ -16,7 +16,7 @@ var friends = [
     },
     {
         name: "friend2",
-        Link: "https://www.google.ca/search?hl=en&biw=1517&bih=741&tbm=isch&sa=1&ei=-8jQXNDSI47r5gKik7GwAw&q=cute+puppie&oq=cute+puppie&gs_l=img.3..0l10.78722.81801..81960...1.0..0.140.1166.10j3......0....1..gws-wiz-img.......0i67.OCPRnDEeNwY#imgrc=8EYxZ3ovn_ol9M:",
+        Link: "/public/Pups.jpg",
         score: [
             "4",
             "2",
@@ -31,7 +31,7 @@ var friends = [
     },
     {
         name: "friend3",
-        Link: "https://www.google.ca/search?hl=en&biw=1517&bih=741&tbm=isch&sa=1&ei=TsnQXPLnM-2O5wL_pZ6oDg&q=cute+hamster&oq=cute+hamster&gs_l=img.3..0l10.12372.16392..16493...0.0..0.116.1017.9j3......0....1..gws-wiz-img.......0i10i67.leRNbcaD89g#imgrc=53Qgi9nsFaIKsM:",
+        Link: "/public/Turts.jpg",
         score: [
             "3",
             "5",
